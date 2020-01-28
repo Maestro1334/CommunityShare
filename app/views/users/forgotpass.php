5edfd8d698210da803ff61e3861f9f529940de6b
Mattis <mattismeeuwesse@gmail.com>
2020-01-28 22:30:36 +0100

    Last changes for forgot/reset password

--- app/views/users/forgotpass.php
+++ app/views/users/forgotpass.php
@@ -1,21 +1,26 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-  <div class="card card-body bg-light mt-5">
-    <div class="row">
-      <div class="col-md-4 mx-auto">
-        <div class="card-heading">
-          <h2 class="card-title">Forgot password</h2>
-        </div>
-        <?php flash('forgot_password_message'); ?>
-        <div class="row card-body">
-          <form action="<?php echo URLROOT; ?>/users/forgotpass" method="POST" class="user-form">
-            <div class="form-group">
-              <label class="label-text" for="email">E-mail address</label>
-              <input type="text" name="email" class="form-control text-box <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
-              <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
+  <div class="card card-body bg-light mt-5 reset-password-container">
+    <div class="col-md-8 mx-auto reset-password-card">
+      <div class="card-heading">
+        <h2 class="card-title forgot-password-title">Forgot password</h2>
+      </div>
+      <?php flash('forgot_password_message'); ?>
+      <div class="row card-body">
+        <form action="<?php echo URLROOT; ?>/users/forgotpass" method="POST" class="user-form">
+          <div class="form-group">
+            <label class="label-text" for="email">E-mail address</label>
+            <input type="text" name="email" class="form-control text-box <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
+            <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <input type="submit" class="btn btn-block btn-success" value="Send email">
             </div>
-            <input type="submit" class="btn btn-block btn-submit" value="Send email">
-          </form>
-        </div>
+            <div class="col-md-6">
+              <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-light btn-block">Back to Login</a>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
--- app/views/users/resetpass.php
+++ app/views/users/resetpass.php
@@ -1,27 +1,25 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-  <div class="card card-body bg-light mt-5">
-    <div class="row">
-      <div class="col-md-4 mx-auto">
-        <div class="card-heading">
-          <h2 class="card-title">Forgot password</h2>
-        </div>
-        <?php flash('token_message'); ?>
-        <div class="row card-body">
-          <form action="<?php echo URLROOT; ?>/users/resetpass" method="POST" class="user-form">
-            <input type="hidden" name="token" value="<?php echo $data['token']; ?>">
-            <div class="form-group">
-              <label class="label-text" for="password">Enter new password</label>
-              <input type="password" name="password" class="form-control text-box <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>" <?php echo (!empty($data['token_err'])) ? 'disabled' : '' ?>>
-              <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
-            </div>
-            <div class="form-group">
-              <label class="label-text" for="confirm_password">Confirm new password</label>
-              <input type="password" name="confirm_password" class="form-control text-box <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" <?php echo (!empty($data['token_err'])) ? 'disabled' : '' ?>>
-              <span class="invalid-feedback"><?php echo $data['confirm_password_err']; ?></span>
-            </div>
-            <input type="submit" class="btn btn-block btn-submit" value="Reset password" <?php echo (!empty($data['token_err'])) ? 'disabled' : '' ?>>
-          </form>
-        </div>
+  <div class="card card-body bg-light mt-5 reset-password-container">
+    <div class="col-md-8 mx-auto reset-password-card">
+      <div class="card-heading">
+        <h2 class="card-title">Forgot password</h2>
+      </div>
+      <?php flash('token_message'); ?>
+      <div class="row card-body">
+        <form action="<?php echo URLROOT; ?>/users/resetpass" method="POST" class="user-form">
+          <input type="hidden" name="token" value="<?php echo $data['token']; ?>">
+          <div class="form-group">
+            <label class="label-text" for="password">Enter new password</label>
+            <input type="password" name="password" class="form-control text-box <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>" <?php echo (!empty($data['token_err'])) ? 'disabled' : '' ?>>
+            <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
+          </div>
+          <div class="form-group">
+            <label class="label-text" for="confirm_password">Confirm new password</label>
+            <input type="password" name="confirm_password" class="form-control text-box <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" <?php echo (!empty($data['token_err'])) ? 'disabled' : '' ?>>
+            <span class="invalid-feedback"><?php echo $data['confirm_password_err']; ?></span>
+          </div>
+          <input type="submit" class="btn btn-block btn-submit" value="Reset password" <?php echo (!empty($data['token_err'])) ? 'disabled' : '' ?>>
+        </form>
       </div>
     </div>
   </div>
